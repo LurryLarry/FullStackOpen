c9c2a027fac5e3f4d8a5a319ef5864add74bb61c
LurryLarry <lari.vesterinen@gmail.com>
2021-02-25 17:01:06 +0200

    2.10 done

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    if (persons.some(entry => entry.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const person = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(person))
+    }
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter
+        handleFilterChange={handleFilterChange}
+        newFilter={newFilter}
+      />
+      <h2>add a new</h2>
+      <PersonForm
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+      />
+      <h2>Numbers</h2>
+      <Persons
+        newFilter={newFilter}
+        persons={persons}/>
+    </div>
+  )
+
+}
+
+export default App
---
+++ osa2/puhelinluettelo/src/components/Persons.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Person from './Person'
+
+const Persons = ({ persons, newFilter }) => {
+  const filterPerson = persons.filter(person => 
+    person.name.toLowerCase().indexOf(newFilter.toLowerCase()) !== -1// jos indexOf antaa positiivisen numeron, se kertoo että elementti on olemassa (argumentti käy toteen). includes antaa boolean arvon..
+  )
+  console.log(filterPerson);
+
+  return (
+    <div>
+    {filterPerson.map((person) =>
+      <Person key={person.name} person={person} />
+    )}
+  </div>
+    )
+  }
+
+export default Persons
