fa45733bba507a99058905794bd65a0094eb51a5
LurryLarry <lari.vesterinen@gmail.com>
2021-05-19 13:26:45 +0300

    5.4

--- osa5/bloglist-frontend/src/components/Notification.js
+++ osa5/bloglist-frontend/src/components/Notification.js
@@ -1,15 +1,43 @@
 import React from 'react'
 
-const Notification = ({ message }) => {
+const Notification = ({ message, success}) => {
+
+  const successStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
+
+  const errorStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
+
   if (message === null) {
     return null
   }
 
-  return (
-    <div className="error">
-      {message}
-    </div>
-  )
-}
+  if (success === true) {
+    return (
+      <div style={successStyle}>
+        {message}
+      </div>
+    )
+  } else {
+    return (
+      <div style={errorStyle}>
+        {message}
+      </div>
+    )}
+  }
 
 export default Notification
