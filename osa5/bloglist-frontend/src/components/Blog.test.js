a16cbefc0e22df773a77740fb29afe642d6f00c2
LurryLarry <lari.vesterinen@gmail.com>
2021-06-17 12:23:15 +0300

    5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
   let component
+  const mockHandler = jest.fn()
+
   const blog = {
     title: 'Meikäläisen blogi',
     author: 'Maija Huu',
     url: 'www.semmone.fi',
-    likes: 10
+    likes: 10,
+    user: {
+      username: 'ukko',
+      name: 'Joku Ukko'
+    }
+  }
+  const user = {
+    user: {
+      username: 'ukko'
+    }
   }
   beforeEach(() => {
     component = render(
-      <Blog blog={blog} />
+      <Blog blog={blog} user={user} toggleVisibility={mockHandler}/>
     )
     component.debug()
   })
@@ -22,5 +33,13 @@ describe('<Blog />', () => {
     expect(
       component.container.querySelector('.onlyTitle')
     ).toBeDefined()
+  })
+
+  test('render all info after button click', async () => {
+    const button = component.container.querySelector('button')
+    fireEvent.click(button)
+
+    const div = component.getByText('www.semmone.fi')
+    expect(div).toHaveStyle('display: block')
   })
 })
