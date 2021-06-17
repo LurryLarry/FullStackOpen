e300695341342329ddd0de3c4f62b48c5d971eef
LurryLarry <lari.vesterinen@gmail.com>
2021-06-17 13:58:31 +0300

    6.1 6.2

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,55 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('reseting works', () => {
+    const action = {
+      type: 'ZERO'
+    }
+
+    const modifiedState = {
+      good: 5,
+      ok: 4,
+      bad: 9
+    }
+    const state = modifiedState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
